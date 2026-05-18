26447fd4feecc6ccabdaecdef690612edeeaa244
rifki-nugraha <syahruldarwin06@gmail.com>
2026-05-18 14:41:34 +0700

    kntl

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 
-Route::apiResource('/posts', PostController::class);
+Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
